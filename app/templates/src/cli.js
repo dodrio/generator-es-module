c9284fb64196daff018121a6cac05fd342478549
m31271n <m31271n@users.noreply.github.com>
2018-07-24 10:46:27 +0800

    style: adjust code style of templates

--- app/templates/src/cli.js
+++ app/templates/src/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const yargs = require('yargs');
-const <%= camelModuleName %> = require('.');
+const yargs = require('yargs')
+const <%= camelModuleName %> = require('.')
 
-const argv = yargs.usage('Usage: <%= repoName %> -w [word]').help('h').argv;
-process.stdout.write(`${<%= camelModuleName %>(argv.w || 'unicorns')}\n`);
+const argv = yargs.usage('Usage: <%= repoName %> -w [word]').help('h').argv
+process.stdout.write(`${<%= camelModuleName %>(argv.w || 'unicorns')}\n`)
